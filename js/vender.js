b97431d89da7bb169cc1f1d780a36447906e0378
language-centerjp <language_center@au.com>
2025-02-20 13:43:27 +0900

    Create vender.js

---
+++ js/vender.js
@@ -0,0 +1,7 @@
+// vendor.js
+
+// ========== jQuery (example) ==========
+// (function(){ /* jQuery code if you want to include it directly */ })();
+
+// ========== Slick Slider ==========
+// (function(){ /* Slick plugin code if needed for a testimonials slider */ })();
